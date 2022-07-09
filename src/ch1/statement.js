03c05f21654c0512449febb2fa14f90e99591fe3
krapshsa <krapshsa@gmail.com>
2022-07-09 20:11:21 +0800

    Extract function 'volumeCreditFor'.

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -11,10 +11,7 @@ function statement(invoice, plays) {
         }).format;
 
     for (let perf of invoice.performances) {
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += volumeCreditFor(perf);
 
         // print line for this order
         result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
@@ -48,5 +45,14 @@ function statement(invoice, plays) {
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
+    }
+
+    function volumeCreditFor(perf) {
+        let volumeCredits = 0;
+
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+
+        return volumeCredits;
     }
 }
