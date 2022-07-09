b472033e8c6560ad456a940439be45793c663382
krapshsa <krapshsa@gmail.com>
2022-07-09 22:11:01 +0800

    Use data.performances instead of invoice.performances.

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -3,13 +3,14 @@ export {statement}
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
     return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
     let result = `Statement for ${(data.customer)}\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         // print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
@@ -68,7 +69,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditFor(perf);
         }
         return result;
@@ -76,7 +77,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
