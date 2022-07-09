bc3715493f982b3a7440329e99dd121cfbd9991b
krapshsa <krapshsa@gmail.com>
2022-07-09 10:08:08 +0800

    first commit

---
+++ test/ch1/statement.test.js
@@ -0,0 +1,42 @@
+import {expect} from 'chai';
+import {statement} from '../../src/ch1/statement.js'
+
+describe('statement', () => {
+    let plays = {
+        "hamlet": {"name": "Hamlet", "type": "tragedy"},
+        "as-like": {"name": "As You Like It", "type": "comedy"},
+        "othello": {"name": "Othello", "type": "tragedy"}
+    };
+
+    let invoices = [
+        {
+            "customer": "BigCo",
+            "performances": [
+                {
+                    "playID": "hamlet",
+                    "audience": 55
+                },
+                {
+                    "playID": "as-like",
+                    "audience": 35
+                },
+                {
+                    "playID": "othello",
+                    "audience": 40
+                }
+            ]
+        }
+    ];
+
+    it('ch1: plain text', () => {
+        let expected = `Statement for BigCo
+ Hamlet: $650.00 (55 seats)
+ As You Like It: $580.00 (35 seats)
+ Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`;
+
+        expect(statement(invoices[0], plays)).to.equal(expected);
+    });
+});
