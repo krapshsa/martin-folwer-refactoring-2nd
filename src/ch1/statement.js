711a08bc21a19676ea26916c2f2063cb8a5914ba
krapshsa <krapshsa@gmail.com>
2022-07-09 20:52:07 +0800

    Rename volumeCredits to result.

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -63,10 +63,10 @@ function statement(invoice, plays) {
     }
 
     function totalVolumeCredits() {
-        let volumeCredits = 0;
+        let result = 0;
         for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditFor(perf);
+            result += volumeCreditFor(perf);
         }
-        return volumeCredits;
+        return result;
     }
 }
