877cf69fd438ae925ad8ba4b087e0408d15830ec
krapshsa <krapshsa@gmail.com>
2022-07-10 10:46:23 +0800

    Move amountFor to statement.

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -10,25 +10,13 @@ function statement(invoice, plays) {
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
-}
-
-function renderPlainText(data) {
-    let result = `Statement for ${(data.customer)}\n`;
-
-    for (let perf of data.performances) {
-        // print line for this order
-        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    }
-
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${(totalVolumeCredits())} credits\n`;
-    return result;
 
     function amountFor(aPerformance) {
         let result = 0;
@@ -51,6 +39,19 @@ function renderPlainText(data) {
         }
         return result;
     }
+}
+
+function renderPlainText(data) {
+    let result = `Statement for ${(data.customer)}\n`;
+
+    for (let perf of data.performances) {
+        // print line for this order
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${(totalVolumeCredits())} credits\n`;
+    return result;
 
     function volumeCreditFor(aPerformance) {
         let result = 0;
@@ -85,7 +86,7 @@ function renderPlainText(data) {
     function totalAmount() {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }
