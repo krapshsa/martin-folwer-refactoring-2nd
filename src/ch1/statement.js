cd217b25cc8ba2be7ca8f0fef227ba59656a7b29
krapshsa <krapshsa@gmail.com>
2022-07-10 12:08:06 +0800

    Implement renderHtml.

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -1,6 +1,6 @@
 import {createStatementData} from "./createStatementData";
 
-export {statement}
+export {statement, htmlStatement}
 
 function statement(invoice, plays) {
     const statementData = createStatementData(invoice, plays);
@@ -19,15 +19,37 @@ function renderPlainText(data) {
     result += `Amount owed is ${usd(data.totalAmount)}\n`;
     result += `You earned ${(data.totalVolumeCredits)} credits\n`;
     return result;
+}
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat(
-            "en-US",
-            {
-                style: "currency",
-                currency: "USD",
-                minimumFractionDigits: 2
-            }
-        ).format(aNumber / 100);
+function htmlStatement(invoice, plays) {
+    const statementData = createStatementData(invoice, plays);
+
+    return renderHtml(statementData);
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${(data.customer)}</h1>\n`;
+    result += '<table>\n';
+    result += '  <tr><th>play</th><th>seats</th><th>cost</th></tr>\n';
+
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${usd(perf.amount)}</td></tr>\n`;
     }
+
+    result += '</table>\n';
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${(data.totalVolumeCredits)}</em> credits</p>\n`;
+
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat(
+        "en-US",
+        {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }
+    ).format(aNumber / 100);
 }
--- test/ch1/statement.test.js
+++ test/ch1/statement.test.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {statement} from '../../src/ch1/statement.js'
+import {statement, htmlStatement} from '../../src/ch1/statement.js'
 
 describe('statement', () => {
     let plays = {
@@ -38,5 +38,20 @@ You earned 47 credits
 `;
 
         expect(statement(invoices[0], plays)).to.equal(expected);
+    });
+
+    it('ch1: html', () => {
+        let expected = `<h1>Statement for BigCo</h1>
+<table>
+  <tr><th>play</th><th>seats</th><th>cost</th></tr>
+  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
+`;
+
+        expect(htmlStatement(invoices[0], plays)).to.equal(expected);
     });
 });
