85d9a7f083622e1dd4fb32a1721c30b94826e2cc
krapshsa <krapshsa@gmail.com>
2022-07-10 10:38:51 +0800

    Remove unused parameters 'invoice' and 'plays'.

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -5,7 +5,7 @@ function statement(invoice, plays) {
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
 
-    return renderPlainText(statementData, invoice, plays);
+    return renderPlainText(statementData);
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
@@ -18,7 +18,7 @@ function statement(invoice, plays) {
     }
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data) {
     let result = `Statement for ${(data.customer)}\n`;
 
     for (let perf of data.performances) {
